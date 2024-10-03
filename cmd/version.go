dd0a246b3b7bbc077a0e4118f8f38b215ef34397
tonmnn <8687434+tonmnn@users.noreply.github.com>
2024-10-03 17:28:06 +0200

    fix unused parameter linting errors

--- cmd/version.go
+++ cmd/version.go
@@ -24,7 +24,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of php-fpm_exporter",
 	Long:  `All software has versions. This is php-fpm_exporter's'`,
-	Run: func(cmd *cobra.Command, args []string) {
+	Run: func(_ *cobra.Command, _ []string) {
 		fmt.Printf("php-fpm_exporter %v\n", Version)
 	},
 }
